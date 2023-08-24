75962acb177f5540b3cc2d962a4ffc98da754fc9
developersrinu <orsusrinu606@gmail.com>
2023-08-24 23:29:10 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
+
+  // Example of using useState
+  let [updateArr, setUpdateArr] = useState('');
+ 
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input type='text' onChange={(e) => setUpdateArr(e.target.value)} />
+      <ul>
+        {fruits.map((item, i) => {
+          if (updateArr === '' || item.includes(updateArr)) {
+            return <li key={i}>{item}</li>;
+          }
+          return null;
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
